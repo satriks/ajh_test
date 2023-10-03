feaeeac9cf284182561cc4a1ff0f148e37db4921
Yury Vereskun <satriks@mail.ru>
2023-10-03 18:36:00 +0300

    first task

--- src/js/__tests__/PaySistem.test.js
+++ src/js/__tests__/PaySistem.test.js
@@ -1,13 +1,13 @@
-import PaySistem from "../componenst/validPaySistem/PaySistem";
+import PaySistem from '../componenst/validPaySistem/PaySistem'
 
 test.each([
-    ['mir', true,'2202235433732147'],  
-    ['undefined', false,'2402235433732147'],  
-    ['visa', true,'4278444840597105'],  
-    ['undefined', false,'42784448405971'],  
-    ['mastercard',true,'5360299162499025'],  
-    ['undefined',false,'8660299162458025'],  
-])('test /s status /s ', (name, status, card ) =>{
-    const result=  new PaySistem(card);
-    expect(result.paySistem).toBe(name)
-}) 
+  ['mir', true, '2202235433732147'],
+  ['undefined', false, '2402235433732147'],
+  ['visa', true, '4278444840597105'],
+  ['undefined', false, '42784448405971'],
+  ['mastercard', true, '5360299162499025'],
+  ['undefined', false, '8660299162458025']
+])('test /s status /s ', (name, status, card) => {
+  const result = new PaySistem(card)
+  expect(result.paySistem).toBe(name)
+})
--- src/js/componenst/algorithmLuhn/algorithmLuhn.js
+++ src/js/componenst/algorithmLuhn/algorithmLuhn.js
@@ -1,44 +1,42 @@
-export default class Luhn{
-    constructor(cardNumber){
-        this.cardNumber = [ ...cardNumber]
-        this.test = 1
-        this.valid = this.isValid()
-    }
+export default class Luhn {
+  constructor (cardNumber) {
+    this.cardNumber = [...cardNumber]
+    this.test = 1
+    this.valid = this.isValid()
+  }
 
-    isValid(){
-        //как я понял карты "Мир" по алгоритму Luhn не проходят, исключил из проверки. 
-        if (this.cardNumber.join('').startsWith('22')){
-            this.valid = true
-            return true
-        }
-        let result = 0
-        for (const index of Array.from(this.cardNumber.keys())){
-            let double =   Number(this.cardNumber[index])
-            let odd = !(index % 2)
-            if (this.cardNumber.length % 2){
-                odd = (index % 2)
-            } 
-            if (odd){
-                double = double * 2
-                if (double > 9){
-                    double -= 9
-                }
-            }
-            result += double    
-            
-        }
-        if (result % 10){
-            this.valid = false
-            return false;
-        }
-        this.valid = true
-        return true
-        
+  isValid () {
+    // как я понял карты "Мир" по алгоритму Luhn не проходят, исключил из проверки.
+    if (this.cardNumber.join('').startsWith('22')) {
+      this.valid = true
+      return true
     }
+    let result = 0
+    for (const index of Array.from(this.cardNumber.keys())) {
+      let double = Number(this.cardNumber[index])
+      let odd = !(index % 2)
+      if (this.cardNumber.length % 2) {
+        odd = (index % 2)
+      }
+      if (odd) {
+        double = double * 2
+        if (double > 9) {
+          double -= 9
+        }
+      }
+      result += double
+    }
+    if (result % 10) {
+      this.valid = false
+      return false
+    }
+    this.valid = true
+    return true
+  }
 }
 
 // var luhn_validate = function(imei){
-//     return !/^\d+$/.test(imei) || (imei.split('').reduce(function(sum, d, n){ 
+//     return !/^\d+$/.test(imei) || (imei.split('').reduce(function(sum, d, n){
 //             return sum + parseInt(((n + imei.length) %2)? d: [0,2,4,6,8,1,3,5,7,9][d]);
 //         }, 0)) % 10 == 0;
 // };
